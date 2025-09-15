547dcd325d3ae92cf20a04f8a2f57b7a91eba253
rrr63 <rrr63@users.noreply.github.com>
2025-09-15 11:17:55 +0000

    [create-pull-request] automated change

--- Formula/adminneo.rb
+++ Formula/adminneo.rb
@@ -1,10 +1,10 @@
 class Adminneo < Formula
   desc "Database management in a single PHP file"
   homepage "https://www.adminneo.org/"
-  url "https://www.adminneo.org/files/5.1.0/__/adminneo-5.1.0.zip"
+  url "https://www.adminneo.org/files/5.1.1/__/adminneo-5.1.1.zip"
   sha256 "4fba21e80933b7c99a5d4fcd152d6d225b04eb1c48516fc0276f63d14e7a9986"
   license "Apache-2.0"
-  version "5.1.0"
+  version "5.1.1"
 
   depends_on "php"
 
